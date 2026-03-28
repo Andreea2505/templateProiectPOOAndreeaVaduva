9ad1f0b66f8d8be58e1c65547124dc01a6f1c18c
Andreea2505 <avaduva150@gmail.com>
2026-03-28 22:22:27 +0200

    Update MedicClient.h

--- OOP/OOP/MedicClient.h
+++ OOP/OOP/MedicClient.h
@@ -4,7 +4,7 @@
 
 class MedicClient : public Client, public MedicVeterinar {
 public:
-    MedicClient(int id, std::string n, std::string t, std::string adr, double s, std::string spec);
+    MedicClient(int id, const std::string& n, std::string t, std::string adr, double s, std::string spec);
     ~MedicClient();
 
     void afiseaza() const;
