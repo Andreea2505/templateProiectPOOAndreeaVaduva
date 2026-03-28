fcd5ed776433a4a8a22ed94cdb6f956346e8e356
Andreea2505 <avaduva150@gmail.com>
2026-03-28 22:18:57 +0200

    Modify Erbivor constructor and override methods

--- OOP/OOP/Erbivor.h
+++ OOP/OOP/Erbivor.h
@@ -5,9 +5,9 @@ class Erbivor : public Mamifer {
 private:
     std::string tipPlante; // tipul de plante preferat
 public:
-    Erbivor(int i, std::string n, unsigned v, Client* p, bool blana, std::string plante);
+    Erbivor(int i, const std::string& n, unsigned v, Client* p, bool blana, std::string plante);
     //mostenite de la mamifer,care le a mostenit de la animal 
-    double CalculeazaCost() override;
-    void Afiseaza() override;
+    double CalculeazaCost()const override;
+    void Afiseaza()const override;
 
 };
