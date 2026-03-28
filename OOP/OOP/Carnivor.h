537a5a80c886f24ffa5d3cf595fdb2a69f8e1210
Andreea2505 <avaduva150@gmail.com>
2026-03-28 15:40:05 +0200

    Remove default destructor from Carnivor class

--- OOP/OOP/Carnivor.h
+++ OOP/OOP/Carnivor.h
@@ -7,6 +7,5 @@ public:
 	Carnivor(int i, std::string n, unsigned v, Client* p, bool blana, std::string carne);
 	double CalculeazaCost() override;
 	void Afiseaza() override;
-~Carnivor() override = default;
 
 };
