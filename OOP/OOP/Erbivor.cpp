840b81266d5c58dc058b0f64858c7e90128b394f
Andreea2505 <avaduva150@gmail.com>
2026-03-28 22:19:28 +0200

    Refactor Erbivor class methods for const correctness

--- OOP/OOP/Erbivor.cpp
+++ OOP/OOP/Erbivor.cpp
@@ -1,18 +1,18 @@
 #include "Erbivor.h"
 #include <iostream>
 
-Erbivor::Erbivor(int i, std::string n, unsigned v, Client* p, bool blana, std::string plante)
+Erbivor::Erbivor(int i, const std::string& n, unsigned v, Client* p, bool blana, std::string plante)
     : Mamifer(i, n, v, p, blana) {
     tipPlante = plante;
 }
 
-double Erbivor::CalculeazaCost() {
+double Erbivor::CalculeazaCost()const {
     double costBaza = Mamifer::CalculeazaCost();
     // erbivorele sunt mai ieftine cu 10%
     return costBaza * 0.9;
 }
 
-void Erbivor::Afiseaza() {
+void Erbivor::Afiseaza()const {
     Mamifer::Afiseaza();
     std::cout << "Tip plante: " << tipPlante << "\n";
 }
