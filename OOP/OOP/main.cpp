045faab3dcc48859083aa1be8091500150505eec
Andreea2505 <avaduva150@gmail.com>
2026-03-28 16:05:05 +0200

    Update main.cpp

--- OOP/OOP/main.cpp
+++ OOP/OOP/main.cpp
@@ -1,10 +1,21 @@
 #include "Meniu.h"
+#include "Exceptii.h"
+#include <iostream>
 
 int main() {
-    Meniu* meniu = Meniu::getInstanta();
-
-    meniu->ruleaza();
-
+    try {
+        Meniu* meniu = Meniu::getInstanta();
+        meniu->ruleaza();
+    }
+    catch (const UserInputError& e) {
+        std::cout << "Eroare: " << e.what() << "\n";
+    }
+    catch (const std::exception& e) {
+        std::cout << "Exceptie standard: " << e.what() << "\n";
+    }
+    catch (...) {
+        std::cout << "Eroare necunoscuta!\n";
+    }
 
     return 0;
 }
