8f3d23b7aceb4fb21484ee1c3bed60bc42284b8e
Andreea2505 <avaduva150@gmail.com>
2026-03-29 19:00:55 +0300

    Update Persoana.h

--- OOP/OOP/Persoana.h
+++ OOP/OOP/Persoana.h
@@ -10,6 +10,6 @@ public:
     //destructor virtual 
     virtual ~Persoana();
     //const pt ca nu modifica obiectul 
-    void afiseaza() const ;
+    virtual void afiseaza() const ;
     std::string getNume() const;
 };
